942d7bbaa70cdab24c517317d0b098ac9ddebb6c
pfeifferjack <pfeifferjack@users.noreply.github.com>
2024-11-02 22:41:20 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,115 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 May 2024 20:48:48 GMT
+// Generated on Sat, 02 Nov 2024 22:41:20 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsEmpty = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
-    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSemiRed = UIColor(red: 1.000, green: 0.373, blue: 0.016, alpha: 0.5019607843137255)
-    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let baseNeutral100 = #fefefeff
+    public static let baseNeutral1000 = #001625ff
+    public static let baseNeutral200 = #f6fafdff
+    public static let baseNeutral300 = #edf5faff
+    public static let baseNeutral400 = #add1e9ff
+    public static let baseNeutral700 = #00548bff
+    public static let baseNeutral800 = #00406bff
+    public static let baseNeutral900 = #002b48ff
+    public static let borderBrand = #25a1f4ff
+    public static let borderDanger = #de8583ff
+    public static let borderInfo = #ae93c4ff
+    public static let borderNeutralMedium = #00406bff
+    public static let borderNeutralStrong = #00548bff
+    public static let borderNeutralWeak = #002b48ff
+    public static let borderSecondary = #f4a925ff
+    public static let borderSuccess = #68ad6bff
+    public static let borderTertiary = #9399d8ff
+    public static let borderWarning = #ba9a2aff
+    public static let brandSemanticBrand300 = #e9f1f7ff
+    public static let brandSemanticBrand500 = #25a1f4ff
+    public static let brandSemanticBrand600 = #0070baff
+    public static let brandSemanticBrand800 = #1f4e80ff
+    public static let brandSemanticDanger300 = #fcf2f2ff
+    public static let brandSemanticDanger500 = #de8583ff
+    public static let brandSemanticDanger600 = #c83734ff
+    public static let brandSemanticDanger800 = #731f1dff
+    public static let brandSemanticInfo300 = #f6f3f8ff
+    public static let brandSemanticInfo500 = #ae93c4ff
+    public static let brandSemanticInfo600 = #855ba6ff
+    public static let brandSemanticInfo800 = #4e3163ff
+    public static let brandSemanticSecondary300 = #f9edd2ff
+    public static let brandSemanticSecondary500 = #f4a925ff
+    public static let brandSemanticSecondary600 = #f3aa29ff
+    public static let brandSemanticSecondary800 = #60481cff
+    public static let brandSemanticSuccess300 = #eff6f0ff
+    public static let brandSemanticSuccess500 = #68ad6bff
+    public static let brandSemanticSuccess600 = #3d7a3fff
+    public static let brandSemanticSuccess800 = #224524ff
+    public static let brandSemanticTertiary300 = #edeef3ff
+    public static let brandSemanticTertiary500 = #9399d8ff
+    public static let brandSemanticTertiary600 = #5b64c3ff
+    public static let brandSemanticTertiary800 = #2e357eff
+    public static let brandSemanticWarning300 = #fff4cbff
+    public static let brandSemanticWarning500 = #ba9a2aff
+    public static let brandSemanticWarning600 = #f1c837ff
+    public static let brandSemanticWarning800 = #493d11ff
+    public static let grayStyle = UIColor(red: 0.541, green: 0.518, blue: 0.518, alpha: 1)
+    public static let percentagesBrandP1 = #e0f3ff1a
+    public static let percentagesBrandP2 = #e0f3ff33
+    public static let percentagesBrandP3 = #e0f3ff66
+    public static let percentagesBrandP4 = #e0f3ff80
+    public static let percentagesBrandP5 = #e0f3ffb3
+    public static let percentagesBrandP6 = #e0f3ffff
+    public static let percentagesComboP1 = #000c141a
+    public static let percentagesComboP2 = #000c1433
+    public static let percentagesComboP3 = #000c1466
+    public static let percentagesComboP4 = #000c1480
+    public static let percentagesComboP5 = #000c14b3
+    public static let percentagesComboP6 = #000c14ff
+    public static let percentagesMixP1 = #73828c1a
+    public static let percentagesMixP2 = #73828c33
+    public static let percentagesMixP3 = #73828c66
+    public static let percentagesMixP4 = #73828c80
+    public static let percentagesMixP5 = #73828cb3
+    public static let percentagesMixP6 = #73828cff
+    public static let percentagesNeutralP1 = #ffffff1a
+    public static let percentagesNeutralP2 = #ffffff33
+    public static let percentagesNeutralP3 = #ffffff66
+    public static let percentagesNeutralP4 = #ffffff80
+    public static let percentagesNeutralP5 = #ffffffb3
+    public static let percentagesNeutralP6 = #ffffffff
+    public static let statesDisabled = #73828cb3
+    public static let statesFocused = #73828c66
+    public static let statesHover = #73828c33
+    public static let statesPressed = #73828c1a
+    public static let surface01 = #00548bff
+    public static let surface02 = #00406bff
+    public static let surface03 = #002b48ff
+    public static let surface04 = #001625ff
+    public static let surfaceBrandStrong = #25a1f4ff
+    public static let surfaceBrandWeak = #1f4e80ff
+    public static let surfaceDangerStrong = #de8583ff
+    public static let surfaceDangerWeak = #731f1dff
+    public static let surfaceInfoStrong = #ae93c4ff
+    public static let surfaceInfoWeak = #4e3163ff
+    public static let surfaceSecondaryStrong = #f4a925ff
+    public static let surfaceSecondaryWeak = #60481cff
+    public static let surfaceSuccessStrong = #68ad6bff
+    public static let surfaceSuccessWeak = #224524ff
+    public static let surfaceTertiaryStrong = #9399d8ff
+    public static let surfaceTertiaryWeak = #2e357eff
+    public static let surfaceWarningStrong = #ba9a2aff
+    public static let surfaceWarningWeak = #493d11ff
+    public static let textBrand = #25a1f4ff
+    public static let textDanger = #de8583ff
+    public static let textInfo = #ae93c4ff
+    public static let textNeutralAStrong = #ffffffff
+    public static let textNeutralAWeak = #000c14ff
+    public static let textNeutralBStrong = #ffffffb3
+    public static let textNeutralBWeak = #000c14b3
+    public static let textNeutralCStrong = #ffffff80
+    public static let textNeutralCWeak = #000c1480
+    public static let textSecondary = #f4a925ff
+    public static let textSuccess = #68ad6bff
+    public static let textTertiary = #9399d8ff
 }
